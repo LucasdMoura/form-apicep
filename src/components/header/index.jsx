da41007d0e6f4d2fdb34f15e00a8b854387fe058
LucasdMoura <lucasmr910@gmail.com>
2024-08-03 22:54:23 -0300

    Correção de responsividade no header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,26 +3,24 @@ import Image from 'next/image'
 export default function Header({children}) {
     return (
         <div>
-            <header className="bg-blue-950 text-white w-full flex items-center h-24 space-x-10 ">
-            <Image 
-            src="/logo.png" 
-            alt="Logo"
-            title='Logo'
-            width= {50} 
-            height= {50}
-            className="ml-40"
-            />
-            
-                <div className="container  flex items-center">
-                <link rel="icon" type="image/png" href="logo.png"/>
-                <nav>
-                <ul className="flex space-x-4  font-bold ">
-                    <li><a href="#" className="text-white hover:text-blue-500">Home</a></li>
-                    <li><a href="#" className="text-white hover:text-blue-500">Sobre</a></li>
-                    <li><a href="#" className="text-white hover:text-blue-500">Contacto</a></li>
-                </ul>
-                </nav>
+            <header className="bg-blue-950 text-white w-full flex items-center justify-between p-4 h-auto mb-10 md:h-24">
+                <div className="flex items-center space-x-4">
+                    <Image 
+                        src="/logo.png" 
+                        alt="Logo"
+                        title='Logo'
+                        width={50} 
+                        height={50}
+                        className="ml-4 md:ml-40"
+                    />
                 </div>
+                <nav className="flex items-center ml-8 justify-start flex-1">
+                    <ul className="flex space-x-4 font-bold">
+                        <li><a href="#" className="text-white hover:text-blue-500">Home</a></li>
+                        <li><a href="#" className="text-white hover:text-blue-500">Sobre</a></li>
+                        <li><a href="#" className="text-white hover:text-blue-500">Contacto</a></li>
+                    </ul>
+                </nav>
             </header>
         </div>
     )
